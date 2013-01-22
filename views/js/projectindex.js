d4beb0b7a610512bace56f32a68e320a451d07eb
robertmarchman <robert.l.marchman@dartmouth.edu>
2013-01-22 23:19:44 +1300

    javascript and paper.js musings

--- views/js/projectindex.js
+++ views/js/projectindex.js
@@ -1,5 +1,32 @@
 
 
-$(document).ready(function() {
-    $("#projects").hide()
-})
+var viewsize = view.size;
+
+var atoms = [];
+
+var rand;
+var rad;
+
+for (i = 0; i < 6; i++) {
+    rand = new Point.random();
+    rand.x *= viewsize.width;
+    rand.y *= viewsize.height;
+    rad = 5 + Math.random() * 10; // radius
+    
+    circle = new Path.Circle(rand, rad);
+    circle.fillColor = 'black';
+    console.log(circle);
+    atoms.push(circle);
+}
+
+
+function onFrame(event) {
+    
+}
+
+
+function onMouseUp(event) {
+    for (var i=0; i < atoms.length; i++) {
+        atoms[i].fillColor = 'red';
+    }
+}
